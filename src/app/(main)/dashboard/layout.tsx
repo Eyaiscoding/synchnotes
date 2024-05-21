e9a6a67588156de16708890b79203ff77231611a
Eya Abidi <abidi.eya001@gmail.com>
2024-05-21 19:14:32 +0100

    Fixed Minor Issue

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -9,7 +9,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = async ({ children, params }) => {
   const { data: products, error } = await getActiveProductsWithPrice();
-  if (error) throw new Error();
+  if (error) console.log(error);
   return (
     <main className="flex over-hidden h-screen">
       <SubscriptionModalProvider products={products}>
